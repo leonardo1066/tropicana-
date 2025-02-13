b73855b02e0b3c9d0892dd7ad3abdc8bbad61a2b
leonardo1066 <132963465+leonardo1066@users.noreply.github.com>
2025-02-13 20:02:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const aumentaFonteBotao = document.getElementById('aumentar-Fonte');
+    const diminuiFonteBotao = document.getElementById('diminui-Fonte');
+    
+    let tamanhoAtualFonte = 1;
+
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = ${tamanhoAtualFonte}rem
+   
+    })
+
+    diminuiFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = ${tamanhoAtualFonte}rem
+    })
+
+
+
+})
